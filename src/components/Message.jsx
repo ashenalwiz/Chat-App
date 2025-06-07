3ac6b5627f28c3fe2c6e834b0e3aa78f433a0e69
Ashen Alwis <129192701+ashenalwiz@users.noreply.github.com>
2025-06-07 11:50:28 +0530

    finished up to starting the firebase Auth part

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Message = () => {
     return (
-        <div className="message">
+        <div className="message owner">
             <div className="messageInfo">
                 <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                 <span>Just now</span>
             </div>
             <div className="messageContent">
                 <p>hello</p>
-                {/* <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />     */}
+                <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />    
             </div> 
            
         </div>
